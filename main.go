fefa102a3594698371d5f132882c0dd1421b4260
Eric Liao <rcliao01@gmail.com>
2017-03-04 13:01:48 -0800

    feat: add template parsing example

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+type Game struct {
+	Name string
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world!")
@@ -18,13 +23,24 @@ func helloRouterHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s!", name)
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// parse templates
+	indexTmpl := template.Must(template.ParseFiles("templates/index.html"))
+	games := []Game{
+		{"Tic-tac-toe"},
+		{"Rock paper scissor"},
+	}
+	indexTmpl.Execute(w, struct{ Games []Game }{games})
+}
+
 func main() {
 	// TODO: parse Environment variable for configuration
 	var addr = ":8080"
 
 	// set up router
 	r := mux.NewRouter()
-	r.HandleFunc("/", helloHandler).Methods("GET")
+	r.HandleFunc("/", indexHandler).Methods("GET")
+	r.HandleFunc("/hello", helloHandler).Methods("GET")
 	r.HandleFunc("/hello/{name}", helloRouterHandler).Methods("GET")
 
 	// start up web application
