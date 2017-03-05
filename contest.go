02979c285689a39075a719e722439c3b5bb8cf45
Eric Liao <rcliao01@gmail.com>
2017-03-05 14:50:54 -0800

    feat: add models implementation based on doc

---
+++ contest.go
@@ -0,0 +1,64 @@
+package contest
+
+// ResultType represents enum of the submission result
+type ResultType uint8
+
+const (
+	SUCCESS            ResultType = 0
+	COMPILE_TIME_ERROR ResultType = 1
+	RUNTIME_ERROR      ResultType = 2
+	WRONG_ANSWER       ResultType = 3
+)
+
+// User represent contest participants, judge, admins
+type User struct {
+	ID        string `json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Type      string `json:"type"`
+}
+
+// Team contains a list of parcipants together
+type Team struct {
+	ID      string `json:"id"`
+	Name    string `json:"id"`
+	Members []User `json:"members"`
+	Score   uint32 `json:"score"`
+}
+
+// Attachment represent a single file upload entry
+type Attachment struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Mimetype string `json:"mimetype"`
+	Path     string `json:"-"`
+}
+
+// Question represent a single question
+type Question struct {
+	ID          string       `json:"id"`
+	Description string       `json:"description"`
+	Attachments []Attachment `json:"attachments"`
+}
+
+// Submission represent a source code submission from the partcipants
+type Submission struct {
+	ID         string       `json:"id"`
+	SourceCode []Attachment `json:"sourcecode"`
+	Result     ResultType   `json:"result"`
+	Feedback   string       `json:"feedback"`
+	Question   Question     `json:"question"`
+	Team       Team         `json:"team"`
+}
+
+// Contest wraps all contest related stuff together
+type Contest struct {
+	ID          uint32     `json:"id"`
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+	HostedDate  Date       `json:"hostedDate"`
+	Teams       []Team     `json:"team"`
+	Questions   []Question `json:"questions"`
+}
